0155816997fd69880c3f247262ca45ca7e8db428
mubsharhussain <mubsharhussain79@gmail.com>
2024-08-07 23:18:26 +0500

    create Type_conversion file

---
+++ Type_conversion.py
@@ -0,0 +1,6 @@
+#in this file i will convert data type to an other data type
+
+birth_year=input("Enter your birth year = ")
+age=2024-int(birth_year) #when we take input it is a string value and we want to subtract 2024 which is int and it gives an error for by using int() function convert str in to int value
+
+print("you age is ",age)
