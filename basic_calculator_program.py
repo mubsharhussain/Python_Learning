8d5ae47016492d7f54326bf6dfa93a07b98a8b77
mubsharhussain <mubsharhussain79@gmail.com>
2024-08-07 23:27:14 +0500

    create basic calculator

---
+++ basic_calculator_program.py
@@ -0,0 +1,5 @@
+#in this program i am creating simple calculator that only do add
+f_num=int(input("enter your 1st number = "))
+s_num=int(input("enter your 2nd number = "))
+sum=f_num+s_num
+print("sum is ",sum)
