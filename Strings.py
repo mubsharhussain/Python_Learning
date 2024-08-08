ae37e9a3eaf832169d6e2c5742df929019a502e8
mubsharhussain <mubsharhussain79@gmail.com>
2024-08-09 00:05:14 +0500

    Add String file

---
+++ Strings.py
@@ -0,0 +1,7 @@
+string="My Name is Mubshar Hussain"
+print(string.upper())
+print(string)
+print(string.lower())
+print(string.find("s"))# this method is use to find 
+print(string.replace("is","89"))# this method us use to replace
+print("Mubshar" in string)# this method return true or false 
