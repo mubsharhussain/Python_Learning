1cee5c9397a256b50c8ea0d4d5b5c769df1b3dac
mubsharhussain <mubsharhussain79@gmail.com>
2024-09-01 00:03:24 +0500

    add list_methods file

---
+++ list_methods.py
@@ -0,0 +1,13 @@
+number=[1,2,3,4,5,6]
+number.append(8)
+print(number)
+print(5 in number)
+print(len(number))
+number.insert(0,-8)
+print(number)
+number.remove(3)
+print(number)
+number.clear()
+print(number)
+
+
